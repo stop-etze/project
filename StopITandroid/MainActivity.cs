549624c5970dd0ddc4dba3e502df8b16a3762e57
itay hadad <itay5245@gmail.com>
2021-06-01 18:07:05 +0300

    Added base forms

---
+++ StopITandroid/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Essentials;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Xamarin.Forms;
+
+namespace StopITandroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Platform.Init(this, savedInstanceState);
+
+            bool login = Preferences.ContainsKey("UID"); // TO DO: set login as firebase login action result
+
+            if (login)
+            {
+                SetContentView(Resource.Layout.home_form);
+            }
+            else
+            {
+                SetContentView(Resource.Layout.activity_main);
+                FindViewById(Resource.Id.authButton).Click += (s, e) => SetContentView(Resource.Layout.auth_form);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
