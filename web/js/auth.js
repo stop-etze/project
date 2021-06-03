fb52121f86d65ccd48f428de7aced4577be42d88
ariel tubul <avarti765@gmail.com>
2021-06-03 21:48:50 +0300

    Added firebase-sdk and UI auth

---
+++ web/js/auth.js
@@ -0,0 +1,23 @@
+//<!-- The core Firebase JS SDK is always required and must be listed first -->
+//<script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-app.js"></script>
+
+//<!-- TODO: Add SDKs for Firebase products that you want to use
+//     https://firebase.google.com/docs/web/setup#available-libraries -->
+//<script src="https://www.gstatic.com/firebasejs/8.6.3/firebase-analytics.js"></script>
+
+//<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCyZYEMAR9-jSl4qq5e4AqIUO0V_ZKQO7I",
+    authDomain: "stop-etze.firebaseapp.com",
+    projectId: "stop-etze",
+    storageBucket: "stop-etze.appspot.com",
+    messagingSenderId: "62544996018",
+    appId: "1:62544996018:web:4edb5dae344dc2a9d9cbab",
+    measurementId: "G-FN71JDK4S8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+//</script>
