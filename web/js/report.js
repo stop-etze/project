0d9cc23f7b025e19db76bf3cf63feaeb2b1da307
ariel tubul <avarti765@gmail.com>
2021-06-09 13:55:11 +0300

    Completed report-form

--- web/js/report.js
+++ web/js/report.js
@@ -1,23 +1,30 @@
 function report() {
-	var images_paths = [];
-
-	for(var file in document.getElementById("formFileMultiple").files) {
-		const ref = firebase.storage().ref().child('images');
-
-		ref.put(file).then((snapshot) => {
-			images_paths.append(ref.fullPath);
-		});
-	}
-
 	db.collection("reports").add({
 		attacker_name: document.getElementById("attackername").value,
 		description: document.getElementById("description").value,
 		reporter: "/users/" + firebase.auth().currentUser.uid,
 		location: document.getElementById("where").value,
-		time: db.Timestamp.now(),
-		images: images_paths
-	}).then(() => {
-		location.href = "approve.html";
+		time: firebase.firestore.Timestamp.now()
+	}).then((snapshot) => {
+		var images_paths = [];
+		var storeageRef = firebase.storage().ref();
+		var files = document.getElementById("formFileMultiple").files;
+
+		for(var i = 0; i < files.length; i++) {
+			var ref = storeageRef.child(snapshot.id + "/" + i + files[i].name.split('.').slice(-1)[0]);
+
+			ref.put(file).then(() => {
+				images_paths.append(snapshot.id + "/" + i + files[i].name.split('.').slice(-1)[0]);
+			});
+		}
+
+		while(files.length != images.length);
+
+		db.ref('reports/' + snapshot).set({
+			images: images_paths
+		}).then(function() {
+			location.href = "approve.html";
+		});
 	}).catch((error) => {
 		alert(error);
 	});
