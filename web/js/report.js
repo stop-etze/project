b7c718314c4a1971aed63e82e73e0683d692fbdd
ariel tubul <avarti765@gmail.com>
2021-06-09 14:27:18 +0300

    Fixed report form bug

--- web/js/report.js
+++ web/js/report.js
@@ -11,16 +11,17 @@ function report() {
 		var files = document.getElementById("formFileMultiple").files;
 
 		for(var i = 0; i < files.length; i++) {
-			var ref = storeageRef.child(snapshot.id + "/" + i + files[i].name.split('.').slice(-1)[0]);
+			var filePath = snapshot.id + "/" + i + files[i].name.split('.').slice(-1)[0];
+			var ref = storeageRef.child(filePath);
 
-			ref.put(file).then(() => {
-				images_paths.append(snapshot.id + "/" + i + files[i].name.split('.').slice(-1)[0]);
+			ref.put(files[i]).then(() => {
+				images_paths.append(filePath);
 			});
 		}
 
 		while(files.length != images.length);
 
-		db.ref('reports/' + snapshot).set({
+		db.doc('reports/' + snapshot.id).set({
 			images: images_paths
 		}).then(function() {
 			location.href = "approve.html";
