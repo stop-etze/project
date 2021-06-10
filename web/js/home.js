efa39167efc5b932c85a1b43834a4cfd2a95e4fc
ariel tubul <avarti765@gmail.com>
2021-06-10 16:25:11 +0300

    Redesigned progress-bar on report-form

--- web/js/home.js
+++ web/js/home.js
@@ -1,7 +1,11 @@
 function hideOrShow() {
-    if(document.getElementsByClassName("popup")[0].style.display == "block") {
-        document.getElementsByClassName("popup")[0].style.display = "none";
-    } else {
-        document.getElementsByClassName("popup")[0].style.display = "block";
-    }
+	if(document.getElementsByClassName("popup")[0].style.display == "block") {
+		document.getElementById('progressBar').style.display = "";
+		document.getElementById('reportButton').style.display = "block";
+		document.getElementsByClassName("popup")[0].style.display = "none";
+	} else {
+		document.getElementById('progressBar').style.display = "none";
+		document.getElementById('reportButton').style.display = "none";
+		document.getElementsByClassName("popup")[0].style.display = "block";
+	}
 }
