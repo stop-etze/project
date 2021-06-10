ff12461afb1e729accad84444c51ccb56acf0be6
itay hadad <itay5245@gmail.com>
2021-06-10 23:35:48 +0300

    Updated settings to communicate with server

---
+++ web/js/settings.js
@@ -0,0 +1,25 @@
+function readSettings() {
+    firebase.auth().onAuthStateChanged(() => {
+        db.doc(`users/${firebase.auth().currentUser.uid}`).get().then((doc) => {
+            doc = doc.data();
+
+            document.getElementById("fullname").value = doc.fullname;
+            document.getElementById("phone").value = doc.phone;
+            document.getElementById("email").value = doc.email;       
+        }).catch(function() { });
+    });
+}
+
+function writeSettings(e) {
+    e.preventDefault();
+
+    db.doc(`users/${firebase.auth().currentUser.uid}`).update({
+        fullname: document.getElementById("fullname").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value
+    }).then(() => {
+        alert("credentials updated seccessfuly");
+    }).catch((err) => {
+        alert(err);
+    });
+}
