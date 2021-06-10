8543dd93e75ce45ce58958164806925c348eb4b4
itay hadad <itay5245@gmail.com>
2021-06-10 21:48:41 +0300

    Optimized loadProgress function even more!

--- web/js/home.js
+++ web/js/home.js
@@ -11,10 +11,11 @@ function hideOrShow() {
 }
 
 function loadProgress() {
-	document.getElementById('progressBar').src = `../html/progress-viewer.html?percent=${
-	await db.collection.doc(`users/${firebase.auth().currentUser.uid}`).get().then((doc) => {
-		return doc.data().progress;
-	}).catch(() => {
-		return 0;
-	})}`;
+	firebase.auth().onAuthStateChanged(() => {
+		db.doc(`users/${ firebase.auth().currentUser.uid }`).get().then((doc) => {
+			document.getElementById('progressBar').src = `../html/progress-viewer.html?percent=${doc.data().progress}`;
+		}).catch(() => { 
+			document.getElementById('progressBar').src = `../html/progress-viewer.html?percent=0`;
+		});
+	});
 }
